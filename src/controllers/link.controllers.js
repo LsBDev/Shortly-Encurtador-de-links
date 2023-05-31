7558f281f9a21ed85d34e9843fb7fd0ff85c4ac6
LsB.Dev <prof.luifel@gmail.com>
2023-05-31 20:42:26 -0300

    Função de pegar urls

--- src/controllers/link.controllers.js
+++ src/controllers/link.controllers.js
@@ -1,23 +1,34 @@
 import { customAlphabet } from "nanoid"
 import { createShortLinkDB } from "../repositories/link.repository.js"
-const nanoid = customAlphabet("123456789abcdef", 10)
+import { getLinkDB } from "../repositories/user.repository.js"
+const nanoid = customAlphabet('123456789abcdef', 10)
 
 export async function shortenLink(req, res) {
-  const {url} = req.body
+  const {link} = req.body
   const {user_id} = res.locals // foi guardado numa variável local (na validação de autenticação)
   const shortLink = nanoid()
 
   try {
-    const result = await createShortLinkDB(url, shortLink, user_id)
+    const result = await createShortLinkDB(link, shortLink, user_id)
     res.status(201).send(result.rows[0])
 
   } catch(err) {
-    res.status(500).send(err)
+    res.status(500).send(err.message)
   }
 }
 
 export async function getLink(req, res) {
-  res.send("getLink")
+  const {id} = req.params
+
+  try {
+    const result = await getLinkDB(id)
+    if(result.rowCount === 0) return res.status(404).send({message: "URL não existe!"})
+    res.status(200).send(result.rows[0])
+
+  } catch(err) {
+    res.status(500).send(err)
+  }
+
 }
 export async function openLink(req, res) {
   res.send("openLink")
--- src/repositories/link.repository.js
+++ src/repositories/link.repository.js
@@ -2,9 +2,10 @@ import { db } from "../database/database.connection.js";
 
 export function createShortLinkDB(url, shortLink, user_id) {
   return db.query(`
-    INSERT INTO urls (url, short_url, user_id) 
+    INSERT INTO link (url, short_url, user_id) 
       VALUES ($1, $2, $3) 
       RETURNING id, short_url;`,
     [url, shortLink, user_id]
   )
 }
+// 
