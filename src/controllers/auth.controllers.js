88649ca2795d71d0260fd471f710c444c2dd3517
LsB.Dev <prof.luifel@gmail.com>
2023-05-31 18:21:06 -0300

    Função login feita

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,7 +1,7 @@
-import { db } from "../database/database.connection.js"
 import { v4 as uuid } from "uuid"
 import bcrypt from "bcrypt"
-import { getUserByEmailDB, signUpDB } from "../repositories/auth.repository.js"
+import { getUserByEmailDB, signUpDB } from "./user.controllers.js"
+import { createSessionDB } from "../repositories/auth.repository.js"
 
 //desestruturar as informações do corpo da requisição aqui e passar para cada função de acesso ao DB somente a informação necessária (otimiza o código).
 
@@ -11,12 +11,12 @@ export async function signUp(req, res) {
     const {name, email, password} = req.body
 
     try {
-        //buscar no banco se o email já está cadastrado
+        //buscar no banco se o email já está cadastrado.
         const emailExist = await getUserByEmailDB(email)
         if(emailExist.rowCount !== 0) return res.status(409).send({message: "E-mail já cadastrado!"})
 
         const hash = bcrypt.hashSync(password, 10)
-        //cadastrar na tabela "users"
+        //cadastrar na tabela "users".
         await signUpDB(name, email, hash)
         res.sendStatus(201)
 
@@ -25,23 +25,23 @@ export async function signUp(req, res) {
     }
 }
 
-
 //Função de login
 
 export async function signIn(req, res) {
     const {email, password} = req.body
-    const token = uuid()
-    const auth = {token: token}
 
     try {
-        const user = await db.query(`
-            SELECT id, name, password FROM users WHERE email = $1;
-        `, [email])
-        if(user.rowCount === 0 || user.rows[0].password != password) return res.sendStatus(401)
-        
-        await db.query(`
-            INSERT INTO 
-        `)
+        const user = await getUserByEmailDB(email)
+        if(user.rowCount === 0) return res.status(401).send("Email não cadastrado!")
+
+        //Nessa validação, seria interessante não informar que a "senha" propriamente está incorreta, pois se fosse um hack tentando entrar, pessoa mal intencionada saberia que o email está cadastrado. Talvez pensar em algo que informe menos detalhes sobre o erro, mas que seja informativo para o usuário correto.
+
+        const passwordCorrect = bcrypt.compareSync(password, user.rows[0].password)
+        if(!passwordCorrect) return res.status(401).send("Senha incorreta!")
+
+        const token = uuid()
+        await createSessionDB(user.rows[0].id, token)
+        res.send({token})
 
     } catch(err) {
         res.send(err.message)
--- src/repositories/auth.repository.js
+++ src/repositories/auth.repository.js
@@ -1,19 +1,10 @@
-import { db } from "../database/database.connection.js"
+import { db } from "../database/database.connection.js";
 
-//Se for necessário fazer duas requisições em uma única função de controle, Só fazer duas funções, cada uma com uma requisição, no mesmo arquivo de repositório.
+//Função de inserção de um usuário na tabela de sessões.
 
-//retornar diretamente o resultado da query ou guardar em uma variável (depende da situação lá no controle).
-
-export function getUserByEmailDB(email) {
+export function createSessionDB(user_id, token) {
   return db.query(`
-    SELECT * FROM users WHERE email = $1;
-    `, [email]
-  )  
-}
-
-export function signUpDB(name, email, password) {
-  return db.query(`
-    INSERT INTO users (name, email, password) VALUES ($1, $2, $3);
-    `, [name, email, password]
-  ) 
+    INSERT INTO session (user_id, token)  VALUES ($1, $2);`,
+    [user_id, token]
+  )
 }
